5017268875a2b76b807b6acc7a9c1285a2db27b0
Will Hunt <half-shot@molrams.com>
2016-03-20 13:34:09 +0000

    Added config file support.

---
+++ cmdlistener.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+
+from djimporter import download_youtube
+from matrix_client.client import MatrixClient
+from mpc.mpc import MPCClient
+from time import time, sleep
+
+class CmdListener:
+    rooms = {}
+    mpc = None
+    client = None
+    stream_url = ""
+    def __init__(self,config):
+        self.mpc = MPCClient(config["mpc"]["host"],config["mpc"]["port"])
+
+        try:
+            self.mpc.current()
+        except:
+            raise Exception("An error occured while connecting to the mpd server.")
+
+
+        try:
+            self.client = MatrixClient(config["matrix"]["host"])
+        except:
+            raise Exception("An error occured while connecting to the matrix server!")
+            return
+
+
+        self.stream_url = config["mpc"]["streamurl"]
+        try:
+            self.client.login_with_password(config["matrix"]["user"],config["matrix"]["pass"])
+        except MatrixRequestError as e:
+            print(e)
+            if e.code == 403:
+                print("Bad username or password.")
+                sys.exit(4)
+            else:
+                print("Check your sever details are correct.")
+                sys.exit(3)
+
+        MTX_ROOMS = config["matrix"]["rooms"].split(",")
+
+        for sroom in MTX_ROOMS:
+            room = self.client.join_room(sroom)
+            room.add_listener(self.__on_cmd)
+            self.rooms[room.room_id] = room
+
+    def run(self):
+        self.client.start_listener_thread()
+        while True:
+            sleep(0.1)
+            pass
+
+    def __on_cmd(self,event):
+        if event['type'] == "m.room.message" and event['content']['msgtype'] == "m.text":
+            if event['age'] < 300:
+                body = event['content']['body'].lower()
+                if body.startswith('mpddj:') or body.startswith('!mpddj'):
+                    self.__parse_command(body[6:],event,event['content']['body'][6:])
+                elif body.startswith('MPD DJ:'):
+                    self.__parse_command(body[7:],event,event['content']['body'][7:])
+
+    def __parse_command(self,cmd,event,cmd_regular):
+        cmd = cmd.strip()
+        parts = cmd.split(" ")
+        room = self.rooms[event['room_id']];
+        if parts[0] == "shuffle":
+            self.mpc.shuffle()
+        elif parts[0] == "prev":
+            self.mpc.next()
+        elif parts[0] == "play":
+            self.mpc.play()
+        elif parts[0] == "next":
+            self.mpc.next()
+        elif parts[0] == "current":
+            fname = mpc.current()
+            fname = fname.replace("_"," ").replace(".ogg","")
+            room.send_text(fname)
+        elif parts[0] == "update":
+            self.mpc.update()
+        elif parts[0] == "help":
+            room.send_text("Commands are: play,prev,next,current,help,[youtube url],stream url")
+        elif "youtube.com/" in parts[0]:
+            try:
+                url = cmd_regular.strip().split(" ")[0]
+                f = download_youtube(url)
+            except Exception as e:
+                print(e)
+                room.send_text("Couldn't download the file :(")
+                return;
+            self.mpc.update(True)
+            self.mpc.add(f)
+            pos = len(self.mpc.playlist().split('\n'))-1
+            if pos > 1:
+                room.send_text("Your request has been queued. It currently at position",pos)
+            else:
+                room.send_text("Your request has begun playing")
+            if mpc.current() == '':
+                sleep(0.5)# Allow it to breathe
+                self.mpc.play()
+        elif "stream url" in cmd:
+            room.send_text(self.stream_url)
--- mmdj.py
+++ mmdj.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """ Matrix MPD DJ """
-from matrix_client.client import MatrixClient
-from mpc.mpc import MPCClient
-from os.path import expanduser
-from time import time, sleep
-from djimporter import download_youtube
-
+import configparser
+from os.path import expanduser,exists
+from cmdlistener import CmdListener
+from sys import exit
 # Error Codes:
 # 1 - Unknown problem has occured
 # 2 - Could not find the server.
@@ -14,96 +12,75 @@ from djimporter import download_youtube
 # 11 - Wrong room format.
 # 12 - Couldn't find room.
 
-#TODO: Add a config file
+def default_config(path):
+    config = configparser.ConfigParser()
 
-def on_cmd(event):
-    if event['type'] == "m.room.message" and event['content']['msgtype'] == "m.text":
-        if event['age'] < 300:
-            body = event['content']['body'].lower()
-            if body.startswith('mpddj:') or body.startswith('!mpddj'):
-                parse_command(body[6:],event,event['content']['body'][6:])
-            elif body.startswith('MPD DJ:'):
-                parse_command(body[7:],event,event['content']['body'][7:])
+    config["mpc"] = {}
+    config["matrix"] = {}
 
-def parse_command(cmd,event,cmd_regular):
-    cmd = cmd.strip()
-    parts = cmd.split(" ")
-    room = rooms[event['room_id']];
-    if parts[0] == "shuffle":
-        mpc.shuffle()
-    elif parts[0] == "prev":
-        mpc.next()
-    elif parts[0] == "play":
-        mpc.play()
-    elif parts[0] == "next":
-        mpc.next()
-    elif parts[0] == "current":
-        fname = mpc.current()
-        fname = fname.replace("_"," ").replace(".ogg","")
-        room.send_text(fname)
-    elif parts[0] == "update":
-        mpc.update()
-    elif parts[0] == "help":
-        room.send_text("Commands are: play,prev,next,current,help,[youtube url],stream url")
-    elif "youtube.com/" in parts[0]:
-        try:
-            url = cmd_regular.strip().split(" ")[0]
-            f = download_youtube(url)
-        except Exception as e:
-            print(e)
-            room.send_text("Couldn't download the file :(")
-            return;
-        mpc.update(True)
-        mpc.add(f)
-        pos = len(mpc.playlist().split('\n'))-1
-        if pos > 1:
-            room.send_text("Your request has been queued. It currently at position",pos)
-        else:
-            room.send_text("Your request has begun playing")
-        if mpc.current() == '':
-            sleep(0.5)# Allow it to breathe
-            mpc.play()
-    elif "stream url" in cmd:
-        room.send_text(MPD_STREAMURL)
+    config["mpc"]["host"] = "localhost"
+    config["mpc"]["port"] = "6600"
+    config["mpc"]["streamurl"] = "http://localhost:8000"
+
+    config["matrix"]["host"] = "https://localhost:8448"
+    config["matrix"]["user"] = "username"
+    config["matrix"]["pass"] = "password"
+    config["matrix"]["rooms"] = "#RoomA,#RoomB:localhost,#RoomC"
+
+    with open(path, 'w') as configfile:
+        config.write(configfile)
+
+def read_config(path):
+    config = configparser.ConfigParser()
+    config.read(path)
+    if "mpc" not in config.keys():
+        print("Error, missing mpc section")
+        return False
+
+    keys = ["host","port","streamurl"]
+    for key in keys:
+        if key not in config["mpc"].keys():
+            print("Error, missing",key,"from mpc section")
+            return False
+
+    if "matrix" not in config.keys():
+        print("Error, missing matrix section")
+        return False
+
+    keys = ["host","user","pass","rooms"]
+    for key in keys:
+        if key not in config["matrix"].keys():
+            print("Error, missing",key,"from matrix section")
+            return False
+
+    return True
+
+#Get config
+cfgfile = expanduser("~/.config/mpddj.ini")
+config = None
+if not exists(cfgfile):
+    print("Config file not found, writing a new one")
+    print("Writing to",cfgfile)
+    config = default_config(cfgfile)
+else:
+    print("Reading",cfgfile)
+    if read_config(cfgfile):
+        config = configparser.ConfigParser()
+        config.read(cfgfile)
+    else:
+        print("Cannot start, you have errors in your config file")
+
+try:
+    cmd = CmdListener(config)
+except Exception as e:
+    print("Failed to connect to one or more services.")
+    print("The message was:",e)
+    exit(2)
+cmd.run()
+
 
 MPD_HOST = "localhost"
-
-#f = open(expanduser("~/.config/mpd.pwd"))
-#MPD_HOST = f.read().strip() + MPD_HOST
-#f.close()
-
 MPD_STREAMURL = "http://half-shot.uk:8000/mpd.ogg"
 MTX_HOST = "https://souppenguin.com:8448"
 MTX_USERNAME = "@mpddj:souppenguin.com"
-
-f = open(expanduser("~/.config/mpddj.pwd"))
-MTX_PASSWORD = f.read().strip()
-f.close()
-
 MTX_ROOMS = ["#devtest:souppenguin.com","#offtopic:matrix.org"]
-rooms = {}
-
-mpc = MPCClient(MPD_HOST)
-client = MatrixClient(MTX_HOST)
-
-try:
-    client.login_with_password(MTX_USERNAME,MTX_PASSWORD)
-except MatrixRequestError as e:
-    print(e)
-    if e.code == 403:
-        print("Bad username or password.")
-        sys.exit(4)
-    else:
-        print("Check your sever details are correct.")
-        sys.exit(3)
-
-for sroom in MTX_ROOMS:
-    room = client.join_room(sroom)
-    room.add_listener(on_cmd)
-    rooms[room.room_id] = room
-
-client.start_listener_thread()
-
-while True:
-    sleep(0.1)
-    pass
