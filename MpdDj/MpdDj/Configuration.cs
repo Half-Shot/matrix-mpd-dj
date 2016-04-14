a662b4d997634f3b9c1a4f3c6b69cebc26612382
Will Hunt <half-shot@molrams.com>
2016-04-14 15:36:47 +0100

    Added config parsing and began work on command listener.

---
+++ MpdDj/MpdDj/Configuration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using IniParser;
+using IniParser.Model;
+namespace MpdDj
+{
+	public class Configuration
+	{
+		public static IniData Config { get; private set; }
+		public static IniData DefaultConfiguration(){
+			IniData defaultData = new IniData ();
+			SectionData MPC = new SectionData ("mpc");
+			SectionData Matrix = new SectionData ("matrix");
+
+			defaultData.Sections.Add (MPC); 
+			defaultData.Sections.Add (Matrix); 
+
+			MPC.Keys.AddKey("host","localhost");
+			MPC.Keys.AddKey("port","6600");
+			MPC.Keys.AddKey("streamurl","http://localhost:8000");
+			MPC.Keys.AddKey("music_dir","/var/lib/mpd/music");
+
+			Matrix.Keys.AddKey("host","https://localhost:8448");
+			Matrix.Keys.AddKey("user","username");
+			Matrix.Keys.AddKey("pass","password");
+			Matrix.Keys.AddKey("rooms","#RoomA,#RoomB:localhost,#RoomC");
+			return defaultData;
+		}
+
+		public static void ReadConfig(string cfgpath){
+			if (File.Exists (cfgpath)) {
+				FileIniDataParser parser = new FileIniDataParser ();
+				Config = parser.ReadFile (cfgpath);
+			} else {
+				Console.WriteLine ("[Warn] The config file could not be found. Using defaults");
+				Config = DefaultConfiguration ();
+			}
+		}
+	}
+}
+
--- MpdDj/MpdDj/Program.cs
+++ MpdDj/MpdDj/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
-
+using MatrixSDK.Client;
+using MatrixSDK.Structures;
 namespace MpdDj
 {
 	class MainClass
 	{
+		public static MatrixClient client;
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			Console.WriteLine ("Reading INI File");
+			string cfgpath;
+			if (args.Length > 1) {
+				cfgpath = System.IO.Path.GetFullPath (args [1]);
+			} else {
+				cfgpath = "~/.config/mpddj.ini";
+			}
+			Configuration.ReadConfig (cfgpath);
+
+			Console.WriteLine ("Connecting to Matrix");
+			client = new MatrixClient (Configuration.Config ["mpc"] ["host"]);
+			Console.WriteLine("Connected. Logging in");
+			client.LoginWithPassword (Configuration.Config ["mpc"] ["user"], Configuration.Config ["mpc"] ["pass"]);
+			Console.WriteLine("Logged in OK");
+			Console.WriteLine("Joining Rooms:");
+			foreach (string roomid in Configuration.Config ["mpc"] ["rooms"].Split(',')) {
+				MatrixRoom room = client.JoinRoom (roomid);
+				room.OnMessage += Room_OnMessage;
+				Console.WriteLine("\tJoined " + roomid);
+			}
+
+		}
+
+		static void Room_OnMessage (MatrixRoom room, MatrixSDK.Structures.MatrixEvent evt)
+		{
+			if (evt.age > 3000) {
+				return; // Too old
+			}
+			string msg = ((MatrixMRoomMessage)evt.content).body;
+
+			if (msg.StartsWith ("!mpddj")) {
+				msg = msg.Substring (7);
+				Console.WriteLine ("Got message okay");
+			}
+
 		}
 	}
 }
